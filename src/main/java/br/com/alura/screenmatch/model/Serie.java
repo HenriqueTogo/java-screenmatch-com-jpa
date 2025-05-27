a16d871a797ec4924e781159e8a71c1b3254aaa4
HenriqueTogo <henrique.togo@gmail.com>
2025-05-27 17:40:29 -0300

    buscando diversas séries e mostrando a informação dessas séries

---
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -0,0 +1,90 @@
+package br.com.alura.screenmatch.model;
+
+import java.util.OptionalDouble;
+
+public class Serie {
+    private String tiutlo;
+    private Integer totalTemporadas;
+    private Double avaliacao;
+    private Categoria genero;
+    private String atores;
+    private String poster;
+    private String sinopse;
+
+    public Serie(DadosSerie dadosSerie) {
+        this.tiutlo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.parseDouble(dadosSerie.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.poster = dadosSerie.poster();
+        this.sinopse = dadosSerie.sinopse();
+    }
+
+    public String getTiutlo() {
+        return tiutlo;
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public String getAtores() {
+        return atores;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setTiutlo(String tiutlo) {
+        this.tiutlo = tiutlo;
+    }
+
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public void setAtores(String atores) {
+        this.atores = atores;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    @Override
+    public String toString() {
+        return "genero=" + genero +
+                ", tiutlo='" + tiutlo + '\'' +
+                ", totalTemporadas=" + totalTemporadas +
+                ", avaliacao=" + avaliacao +
+                ", atores='" + atores + '\'' +
+                ", poster='" + poster + '\'' +
+                ", sinopse='" + sinopse;
+    }
+}
