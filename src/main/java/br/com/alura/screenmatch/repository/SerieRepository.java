5158f59816239d2f339111613272aef04ff12915
HenriqueTogo <henrique.togo@gmail.com>
2025-06-24 19:19:37 -0300

    implementando queries com JPQL

--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -1,8 +1,11 @@
 package br.com.alura.screenmatch.repository;
 
 import br.com.alura.screenmatch.model.Categoria;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +20,40 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     List<Serie> findByGenero(Categoria categoria);
 
     List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, double avaliacao);
+
+    @Query("""
+            SELECT s
+            FROM Serie s
+            WHERE s.totalTemporadas <= :totalTemporadas
+            AND s.avaliacao >= :avaliacao
+            """
+    )
+    List<Serie> seriesPorTemporadaEAvaliacao(@Param("totalTemporadas") int totalTemporadas, @Param("avaliacao") double avaliacao);
+
+    @Query("""
+            SELECT e
+            FROM Serie s
+            JOIN s.episodios e
+            WHERE e.titulo ILIKE %:trechoEpisodio%
+            """)
+    List<Episodio> episodiosPorTrecho(@Param("trechoEpisodio") String trechoEpisodio);
+
+    @Query("""
+            SELECT e
+            FROM Serie s
+            JOIN s.episodios e
+            WHERE s = :serie
+            ORDER BY e.avaliacao DESC
+            LIMIT 5
+            """)
+    List<Episodio> topEpisodiosPorSerie(@Param("serie") Serie serie);
+
+    @Query("""
+            SELECT e
+            FROM Serie s
+            JOIN s.episodios e
+            WHERE s = :serie
+            AND YEAR(e.dataLancamento) >= :anoLancamento
+            """)
+    List<Episodio> episodioPorSerieEAno(@Param("serie") Serie serie, @Param("anoLancamento") int anoLancamento);
 }
